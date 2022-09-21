f451b8cd1d3fa68be4d2148b57e6524e4dc2b6a2
risalstr <risa.lestari2002@gmail.com>
2022-09-21 23:53:15 +0700

    lab 3 done

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,15 +4,21 @@ from wishlist.views import barang_wishlist_xml
 from wishlist.views import barang_wishlist_json
 from wishlist.views import barang_wishlist_by_id_json
 from wishlist.views import barang_wishlist_by_id_xml
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
 urlpatterns = [
-    path('', show_wishlist, name='show_wishlist'),
-    path('xml/', barang_wishlist_xml, name='barang_wishlist_xml'),
-    path('json/', barang_wishlist_json, name='barang_wishlist_json'),
-    path('json/<int:id>', barang_wishlist_by_id_json,
-         name='barang_wishlist_by_id_json'),
-    path('xml/<int:id>', barang_wishlist_by_id_xml,
-         name='barang_wishlist_by_id_xml'),
+     path('', show_wishlist, name='show_wishlist'),
+     path('xml/', barang_wishlist_xml, name='barang_wishlist_xml'),
+     path('json/', barang_wishlist_json, name='barang_wishlist_json'),
+     path('json/<int:id>', barang_wishlist_by_id_json,
+          name='barang_wishlist_by_id_json'),
+     path('xml/<int:id>', barang_wishlist_by_id_xml,
+          name='barang_wishlist_by_id_xml'),
+     path('register/', register, name='register'),
+     path('login/', login_user, name='login'),
+     path('logout/', logout_user, name='logout')
 ]
